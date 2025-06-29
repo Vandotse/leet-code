205ff3deb5556eb882ea37e7ac0e1bb6bd0661aa
Evan Haba <hippoguy101@gmail.com>
2025-06-28 18:27:57 -0700

    121. Best Time to Buy and Sell Stock Solve

---
+++ maxProfit.go
@@ -0,0 +1,16 @@
+package main
+
+func maxProfit(prices []int) int {
+	maxProfit := 0
+	buy := prices[0]
+	for _, sell := range prices {
+		profit := sell - buy
+
+		if sell < buy {
+			buy = sell
+		} else if profit > maxProfit {
+			maxProfit = profit
+		}
+	}
+	return maxProfit
+}
